f0fee857194df5210841ea9c53c98f584f0abcbd
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-08 17:40:41 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,28 @@ def array_with_two_elements
   dogs = ["Max", "Dougie"]
   return dogs.inspect
 end
+
+def first_element_with_array_methods(array)
+  return array.firt
+end
+
+def third_element(array)
+  return array[3]
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def first_element(array)
+  return array[1]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def length_of_array(array)
+  return array.length
+end
+
